d7378cc6b4562eaa80d766ec0839639bf223e2ce
Luke <fluke.l@gmail.com>
2018-01-21 00:27:30 +0800

    Fix linkage of tensorflow_framework and cxx11 ABI problem.

--- lib/make.sh
+++ lib/make.sh
@@ -1,8 +1,10 @@
 #TF_INC=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_include())')
-TF_INC=$(python -c 'import sys; print([p for p in sys.path if p.find("lib/python")>0 and p.find("site-")>0 and p.find("local")<0][0])')/tensorflow/include
+PY_SITE_PACKAGE=$(python -c 'import sys; print([p for p in sys.path if p.find("lib/python")>0 and p.find("site-")>0 and p.find("local")<0][0])')
+TF_INC=$PY_SITE_PACKAGE/tensorflow/include
+TF_LIB=$PY_SITE_PACKAGE/tensorflow
 
 CUDA_PATH=/usr/local/cuda/
-CXXFLAGS=''
+CXXFLAGS='-D_GLIBCXX_USE_CXX11_ABI=0'
 
 if [[ "$OSTYPE" =~ ^darwin ]]; then
 	CXXFLAGS+='-undefined dynamic_lookup'
@@ -13,14 +15,14 @@ cd roi_pooling_layer
 if [ -d "$CUDA_PATH" ]; then
 	nvcc -std=c++11 -c -o roi_pooling_op.cu.o roi_pooling_op_gpu.cu.cc \
 		-I $TF_INC -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC $CXXFLAGS \
-		-arch=sm_37
+		-arch=sm_61
 
 	g++ -std=c++11 -shared -o roi_pooling.so roi_pooling_op.cc \
 		roi_pooling_op.cu.o -I $TF_INC -I $TF_INC/external/nsync/public  -D GOOGLE_CUDA=1 -fPIC $CXXFLAGS \
-		-lcudart -L $CUDA_PATH/lib64
+		-lcudart -L $CUDA_PATH/lib64 -L$TF_LIB -ltensorflow_framework
 else
 	g++ -std=c++11 -shared -o roi_pooling.so roi_pooling_op.cc \
-		-I $TF_INC -I $TF_INC/external/nsync/public -fPIC $CXXFLAGS
+		-I $TF_INC -I $TF_INC/external/nsync/public -fPIC $CXXFLAGS -L$TF_LIB -ltensorflow_framework
 fi
 
 cd ..
